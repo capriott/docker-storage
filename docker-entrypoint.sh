e179ae25b3ec7c6796bef590f1c494bace018e96
Andrea Capriotti <capriott@gmail.com>
2023-02-12 18:14:01 +0100

    Swith to useradd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,9 +11,8 @@ fi
 
 # create user with uid and gid matching that of the host user
 if ! getent passwd storage >/dev/null; then
-	adduser --uid ${USER_UID} --gid ${USER_GID} \
-		--disabled-login \
-		--gecos 'Firefox user' storage
+	useradd --uid ${USER_UID} --gid ${USER_GID} \
+		-c 'Firefox user' storage
 fi
 
 exec "$@"
